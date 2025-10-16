b85107c21d385a4c16b751d81452eecef3ab3c4b
Ahmed Davila Guerrero <c-aguerrero@kahua.com>
2025-10-16 19:43:12 -0300

    login

--- src/Layout.js
+++ src/Layout.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./index.css";
 import Header from "./Header";
+import axios from "axios";
 
 import { Outlet } from "react-router-dom";
 
 export default function Layout() {
+  useEffect(() => {
+    async function getProfile() {
+      const profileUrl = `${process.env.REACT_APP_HOST}:${process.env.REACT_APP_PORT}/${process.env.REACT_APP_API_VERSION}/profile`;
+
+      try {
+        const response = await axios({
+          url: profileUrl,
+          method: "GET",
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getProfile();
+  }, []);
+
   return (
     <>
       <Header />
